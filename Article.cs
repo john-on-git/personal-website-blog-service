5f8feb7ade2a926096b8f2d9a1fd67f9bfece74a
John <johngreggwebster@gmail.com>
2025-01-18 13:25:43 +0000

    basic w/ test data

--- Article.cs
+++ Article.cs
@@ -1,6 +1,11 @@
 ﻿namespace JGWPersonalWebsiteBlogAPI
 {
-    public record Article(uint Id, string Title, string Authors, string HTMLSnippet)
+    public class Article(string Title, string Authors, string HTMLSnippet)
     {
+        public uint Id { get; private set; } //PK
+        public string Title { get; private set; } = Title;
+        public string Authors { get; private set; } = Authors;
+        public string HTMLSnippet { get; private set; } = HTMLSnippet;
+        public DateTime PostedAt { get; private set; } = DateTime.Now;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,21 +1,53 @@
 using JGWPersonalWebsiteBlogAPI;
 using Microsoft.EntityFrameworkCore;
+using static System.Formats.Asn1.AsnWriter;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<BlogContext>();
 
+
+// Add services to the container.
+builder.Services.AddDbContext<BlogContext>(options => {
+    options
+        .UseSqlite("Data Source=blog.dat;Cache=Shared")
+        .UseSeeding((ctx, _) =>
+        {
+            BlogContext blogCtx = (BlogContext)ctx;
+            if(!blogCtx.Articles.Any()) //insert test articles if they don't already exist
+            {
+                blogCtx.Articles.Add(new Article("Test1", "John", "<p>Hello World!</p>"));
+                blogCtx.Articles.Add(new Article("Test2", "John", "<p>Hello Birds!</p>"));
+                blogCtx.Articles.Add(new Article("Test3", "John", "<p>Hello Sky!!!</p>"));
+                blogCtx.SaveChanges();
+            }
+        });
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.Services.CreateScope().ServiceProvider.GetRequiredService<BlogContext>()?.Database.EnsureCreated(); //create the database with test values if it doesn't already exist
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
 
-app.MapGet("/index", () =>
-{
-    return 
-});
+//set up endpoints
+app.MapGet(
+    $"/index",
+    async (uint? offset, BlogContext db) =>
+    {
+        return offset==null ? 
+            await db.Articles
+                .OrderBy(x => x.Id)
+                .ToListAsync()
+            :
+            await db.Articles
+                .OrderBy(x => x.Id)
+                .Where(x => x.Id > offset)
+                .ToListAsync();
+    }
+);
+
+
+
 
 app.Run();
